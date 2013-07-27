011fccb637944535156db134f2162a2ccb3258d4
surajgillespie <suraj_gillespie@yahoo.co.in>
2013-07-27 23:35:14 +0530

    Patch the bug which causes the parsed HTML content to load only after you make changes in input

--- js/activity.js
+++ js/activity.js
@@ -2,7 +2,7 @@ define(function (require) {
     var activity = require("sugar-web/activity/activity");
 
     require("Markdown.Converter");
-    require("Markdown.Sanitizer");
+    //require("Markdown.Sanitizer");
     require("Markdown.Editor");
 
 
@@ -13,7 +13,7 @@ define(function (require) {
         activity.setup();
 
         inputTextContent = document.getElementById("wmd-input-second");
-
+        inputTextContent.value = "#This is a sample input";
         //to save and resume the contents from datastore.
 
         var datastoreObject = activity.getDatastoreObject();
@@ -24,7 +24,7 @@ define(function (require) {
             datastoreObject.setDataAsText(jsonData);
             datastoreObject.save(function () {});
         };
-
+        markdownParsing(); //to load for the first time
         datastoreObject.loadAsText(function (error, metadata, data) {
             console.log(metadata);
             console.log(data);
@@ -33,48 +33,28 @@ define(function (require) {
             console.log(markdowntext);
 
             inputTextContent.value = markdowntext;
+            markdownParsing(); //to load again when there is a datastore entry
         });
 
-        /*
-                var converter1 = Markdown.getSanitizingConverter();
-                
-                converter1.hooks.chain("preBlockGamut", function (text, rbg) {
-                    return text.replace(/^ {0,3}""" *\n((?:.*?\n)+?) {0,3}""" *$/gm, function (whole, inner) {
-                        return "<blockquote>" + rbg(inner) + "</blockquote>\n";
-                    });
-                });
-                
-                var editor1 = new Markdown.Editor(converter1);
-                
-                editor1.run();
-                */
-        var converter2 = new Markdown.Converter();
+        function markdownParsing() {
+            var converter2 = new Markdown.Converter();
 
-        /*
-                converter2.hooks.chain("preConversion", function (text) {
-                    return text.replace(/\b(a\w*)/gi, "*$1*");
-                });
-                
-                converter2.hooks.chain("plainLinkText", function (url) {
-                    return "This is a link to " + url.replace(/^https?:\/\//, "");
-                });
-                */
-
-        var help = function () {
-            alert("Do you need help?");
-        }
-        var options = {
-            helpButton: {
-                handler: help
-            },
-            strings: {
-                quoteexample: "whatever you're quoting, put it right here"
+            var help = function () {
+                alert("Do you need help?");
             }
-        };
-        
-        var editor2 = new Markdown.Editor(converter2, "-second", options);
+            var options = {
+                helpButton: {
+                    handler: help
+                },
+                strings: {
+                    quoteexample: "whatever you're quoting, put it right here"
+                }
+            };
 
-        editor2.run();
+            var editor2 = new Markdown.Editor(converter2, "-second", options);
+
+            editor2.run();
+        }
 
     });
 
